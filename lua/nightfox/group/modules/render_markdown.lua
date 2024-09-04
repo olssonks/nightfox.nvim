340b999897b59bb03af55bc78a61b10960f3a857
K. S. Olsson <62556548+olssonks@users.noreply.github.com>
2024-09-04 10:28:34 -0400

    Update render_markdown.lua

--- lua/nightfox/group/modules/render_markdown.lua
+++ lua/nightfox/group/modules/render_markdown.lua
@@ -21,7 +21,7 @@ function M.get(spec, config, opts)
   }
 
   for i, color in ipairs(rainbow) do
-    local new_bg = tostring(C.from_hex(color.base):shade(-0.3))
+    local new_bg = C.from_hex(color.base):shade(-0.3)
     print(new_bg:to_hex())
     groups["RenderMarkdownH" .. i .. "Bg"] = { bg = new_bg:to_hex() }
     groups["RenderMarkdownH" .. i .. "Fg"] = { fg = color.bright, bold = true }
